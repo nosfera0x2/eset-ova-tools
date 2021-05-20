fbe2ca370f814b7eaaa8d20b8db0066885789cff
spence-rat <82817752+spence-rat@users.noreply.github.com>
2021-05-19 20:24:23 -0500

    Add files via upload

---
+++ server-lynx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+function install_lynx(){
+	if yum list installed "lynx" >/dev/null 2>&1; then
+		echo "lynx already installed"
+	else	
+		yum install lynx
+	fi
+	}
+install_lynx
+lynx /var/log/eset/RemoteAdministrator/Server/status.html
