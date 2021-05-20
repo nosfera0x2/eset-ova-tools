991992f05444563af0c500d6546fedb023f4247e
spence-rat <82817752+spence-rat@users.noreply.github.com>
2021-05-19 20:12:17 -0500

    Delete whiptail-testing.sh

--- whiptail-testing.sh
+++
@@ -1,6 +0,0 @@
-whiptail --title "Menu example" --menu "Choose an option" 25 78 16 \
-"<-- Back" "Return to the main menu." \
-"Server Trace Log" "Viewed in readable html format" \
-"Uninstall Webconsole" "Uninstalls webconsole" \
-"Install Webconsole "Installs Webconsole (http only)" \
-"Ping Module Updat Server" "Outputs text file." 
